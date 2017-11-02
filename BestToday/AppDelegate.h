687704470dfe0f05017161cd923eec7305010ccf
leeco <wangfaquan@le.com>
2017-11-02 09:37:28 +0800

    Initial Commit

---
+++ BestToday/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BestToday
+//
+//  Created by leeco on 2017/11/2.
+//  Copyright © 2017年 leeco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
